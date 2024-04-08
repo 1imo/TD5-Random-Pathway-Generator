e718b79671001f6d24237e343e21473f3ea8d4cc
1imo <100065732+1imo@users.noreply.github.com>
2024-04-08 23:22:23 +0100

    Upload

---
+++ main.py
@@ -0,0 +1,23 @@
+import sys
+import random
+import data_classes
+
+# The information has been stored in a separate class (procedural abstraction)
+info = data_classes.Information()
+
+# Shorthand declaration of the arguments
+# Amount of towers to be generated pp, names of the players+ (although there are no strict checks (an error will occur))
+args = sys.argv
+
+# Counter. The first name provided will be at index 2
+index = 2
+
+while len(args) > index:
+    print(args[index])  # Print the name of the player
+    for i in range(int(args[1])):
+        print(random.choice(info.playable_characters))  # Print the random tower
+    print()  # Print a newline (for readability)
+    index = index + 1  # Increment the index
+
+print(" ".join(random.choice(info.maps)))  # Print a random map
+print(random.choice(info.difficulties))  # Print a random difficulty
